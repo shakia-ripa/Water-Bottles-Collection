7609a275b3df71d1b3e42fcec7b81197089e2347
shakia-ripa <shakiaripa@gmail.com>
2023-09-14 22:19:49 +0600

    implemented feature for showing cart items

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,8 +1,20 @@
 import PropTypes from 'prop-types';
+import './Cart.css'
 const Cart = ({cart}) => {
     return (
         <div>
             <h3>Cart: {cart.length}</h3>
+            <div className='cart-container'>
+                {
+                    cart.map((bottle) => <div key={bottle.id}>
+                        <img src={bottle.img}></img>
+                        <br />
+                        <button>Remove</button>
+                        </div>)
+                    
+                }
+                
+            </div>
         </div>
     );
 };
