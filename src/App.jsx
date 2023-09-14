dfc4b43b8104d06d46fdf26b52fd833920d9ef23
shakia-ripa <shakiaripa@gmail.com>
2023-09-14 21:02:08 +0600

    Project initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Bottles from './Components/Bottles/Bottles'
+
+function App() {
+
+  return (
+    <>
+      <h1>Water Bottles</h1>
+      <Bottles></Bottles>
+    </>
+  )
+}
+
+export default App
